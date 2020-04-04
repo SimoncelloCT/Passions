f32ad037b86248b20cf3479e192fd6186df7241e
Simone Scionti <simoncello98@yahoo.it>
2020-04-04 14:01:05 +0200

    Initial Commit

---
+++ Passions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Passions
+//
+//  Created by Simone Scionti on 04/04/2020.
+//  Copyright © 2020 SSCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
